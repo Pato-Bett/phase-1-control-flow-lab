1855561c9c209a41bc58813ac3616a34892d87ea
Pato-Bett <patobett588@gmail.com>
2023-10-04 11:38:51 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance>=0&&distance<=400) {
+    return ('This one is on me!');
+  } 
+  else if(distance>2000&&distance<=2500){
+    return ('I will gladly take your thirty bucks.');
+  }
+  else{
+    return ('No can do.'); 
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  // let response=destination==='NYC'? ('Ok, sounds good.'):('No go.');
+  // return response;
+  return destination==='NYC'?'Ok, sounds good.':'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+  switch (tip) {
+    case ('generous'):
+      return response='Thank you so much.';
+      break;
+    case ('not as generous'):
+      return response='Thank you.';
+    default:
+      return response='Bye.';
+      break;
+  }
 }
